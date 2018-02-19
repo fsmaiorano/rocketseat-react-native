caf6d4ff626d9f3078b76b6f07c20d4004773e0c
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-02-19 15:03:25 -0300

    add: devtoolsConfig, ReactotronConfig and babel module resolver

---
+++ module_1/challenge/src/index.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+import 'config/DevToolsConfig';
+import 'config/ReactotronConfig';
+
+export default class App extends Component {
+    render() {
+        return(
+            <View>
+                <Text>abc</Text>
+            </View>
+        )
+    }
+}
