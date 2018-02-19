ebe1ce02b981fe5673b32ebdd07c48ab3cdf9a18
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-02-19 15:45:07 -0300

    add: post and listPost components

---
+++ module_1/challenge/src/components/post/listPost.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+import Post from './post';
+
+const ListPost = () => (
+  <View>
+    <Text>ListPost</Text>
+    <Post />
+  </View>
+)
+
+export default ListPost;
--- module_1/challenge/src/index.js
+++ module_1/challenge/src/index.js
@@ -4,11 +4,14 @@ import {View, Text, StyleSheet} from 'react-native';
 import 'config/DevToolsConfig';
 import 'config/ReactotronConfig';
 
+import ListPost from 'components/post/listPost';
+
 export default class App extends Component {
     render() {
         return(
             <View>
                 <Text>abc</Text>
+                <ListPost />
             </View>
         )
     }
