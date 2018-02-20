e86c9c02dbe0b6ea1bcb26fd5ea8bae637178897
fabio.maiorano <fabio.maiorano@D-CB-001330>
2018-02-20 15:48:14 -0300

    update: styles on post component

--- module_1/challenge/src/components/post/listPost.js
+++ module_1/challenge/src/components/post/listPost.js
@@ -1,35 +1,15 @@
 import React from 'react';
-import { bgPost } from 'utils/colors';
-import { ScrollView, StyleSheet, Platform } from 'react-native';
-
+import { ScrollView } from 'react-native';
 import Post from './post';
 
 const ListPosts = props => (
   <ScrollView>
     {
       props.posts.map(post => (
-        <Post style={styles.postItem} key={post.id} post={post} />
+        <Post key={post.id} post={post} />
       ))
     }
   </ScrollView>
 );
-
-const styles = StyleSheet.create({
-  postItem: {
-    backgroundColor: bgPost,
-    borderRadius: Platform.OS === 'ios' ? 16 : 2,
-    padding: 20,
-    marginLeft: 10,
-    marginRight: 10,
-    marginTop: 17,
-    justifyContent: 'center',
-    shadowRadius: 3,
-    shadowOpacity: 0.8,
-    shadowOffset: {
-      width: 0,
-      height: 3,
-    },
-  },
-});
 
 export default ListPosts;
--- module_1/challenge/src/components/post/post.js
+++ module_1/challenge/src/components/post/post.js
@@ -1,16 +1,31 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { bgPost } from 'utils/colors';
 
 const Post = props => (
-  <View>
+  <View style={styles.postItem}>
     <Text>{props.post.title}</Text>
     <Text>{props.post.author}</Text>
     <Text>{props.post.content}</Text>
   </View>
 );
 
-// const styles = StyleSheet.create({
-
-// });
+const styles = StyleSheet.create({
+  postItem: {
+    marginLeft: 10,
+    marginRight: 10,
+    marginTop: 50,
+    borderRadius: 10,
+    backgroundColor: bgPost,
+    justifyContent: 'center',
+    padding: 20,
+    shadowRadius: 3,
+    shadowOpacity: 0.8,
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+  }
+});
 
 export default Post;
